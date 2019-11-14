14f57856742ba795bf9859c8b020c78b9cc53856
Courtney Buratto <courtneyburatto@Courtneys-MacBook-Pro.local>
2019-11-14 13:00:45 -0700

    Created ref for canvas element

--- src/components/Game/Graph/index.js
+++ src/components/Game/Graph/index.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import './Graph.css'
 
-function Graph(props) {
-	return (
-		<div>
-			<canvas className='canvas'></canvas>
-		</div>
-	)
+
+
+class Graph extends React.Component {
+	constructor(props) {
+		super(props)
+		this.canvasRef = React.createRef()
+	}
+
+	componentDidMount() {
+		const canvas = this.canvasRef.current
+		const context = canvas.getContext('2d')
+		context.rect(0, 0, canvas.width, canvas.height)
+	}
+
+	render() {
+		return (
+			<div>
+				<canvas className = 'canvas' ref={this.canvasRef} />
+			</div>
+		)
+	}
 }
 
 export default Graph

19e917c8b9a8d89e1093eef1e6e6842a55eb0b8b
Courtney Buratto <courtneyburatto@Courtneys-MacBook-Pro.local>
2019-11-14 19:06:16 -0700

    Canvas

--- src/components/Game/Graph/index.js
+++ src/components/Game/Graph/index.js
@@ -13,6 +13,14 @@ class Graph extends React.Component {
 		const canvas = this.canvasRef.current
 		const context = canvas.getContext('2d')
 		context.rect(0, 0, canvas.width, canvas.height)
+		for (let i = 0; i < 20; i++) {
+			for (let j = 0; j < 20; j++) {
+				context.beginPath();
+				context.fillStyle = ["#FFFFFF", "#343434"][(i + j) % 2];
+				context.fillRect(j * 30, i * 30, 30, 30);
+				context.closePath();
+			}
+		}
 	}
 
 	render() {

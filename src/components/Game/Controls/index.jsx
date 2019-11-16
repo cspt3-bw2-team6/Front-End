0edcdcab057ed33ff92b65da08c12dd4640fcc24
robsalzberg <robsalzberg@gmail.com>
2019-11-16 14:24:55 -0500

    Merge

--- src/components/Game/Controls/index.jsx
+++ src/components/Game/Controls/index.jsx
@@ -16,16 +16,34 @@ function Controls(props) {
         </div>
         <Button direction="e" move={props.move} />
       </div>
-      <button type="button" onClick={props.autoTraversal} id="automate" className="nes-btn">Automate</button>
-    <div>
-    <button type="button" onClick={props.takeit} className="nes-btn" />
-    <button type="button" onClick={props.dropit} className="nes-btn" />
-    <button type="button" onClick={props.praying} className="nes-btn" />
-    <button type="button" className="nes-btn" onClick={props.examine} />
-    </div>
-    <TextInput propFunction={props.examine} label="Item or Player Name" activity="Examine" />
+      <button
+        type="button"
+        onClick={props.autoTraversal}
+        id="automate"
+        className="nes-btn"
+      >
+        Automate
+      </button>
+      <div>
+        <button type="button" onClick={props.takeit} className="nes-btn" />
+        <button type="button" onClick={props.dropit} className="nes-btn" />
+        <button type="button" className="nes-btn" onClick={props.examine} />
+      </div>
+      <TextInput
+        primaryFunction={props.examine}
+        label="Item or Player Name"
+        activity1="Examine"
+      />
+      <TextInput
+        label={"Item Name"}
+        activity1={"Carry"}
+        activity2={"Receive"}
+        primaryFunction={() => console.log("carry")}
+        secondaryFunction={() => console.log("recieve")}
+      />
     </div>
   );
 }
 
 export default Controls;
+

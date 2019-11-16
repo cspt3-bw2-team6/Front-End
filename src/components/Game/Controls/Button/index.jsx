e8a4b10f82bc8387ce800f6aa5d960c4150f3ecc
Jonathan Dorety <kyullog@gmail.com>
2019-11-16 12:29:19 -0800

    Direction buttons display cardinal directions

--- src/components/Game/Controls/Button/index.jsx
+++ src/components/Game/Controls/Button/index.jsx
@@ -3,27 +3,19 @@ import "nes.css/css/nes.min.css";
 import "./Button.css";
 
 function Button(props) {
-  const dir = {
-    w: "&#11207",
-    e: "&#11208",
-    n: "&#11205",
-    s: "&#11206"
-  };
 
   const clickHandler = () => {
     props.move(props.direction);
   };
 
-  const setDirection = () => {
-    return { __html: dir[props.direction] };
-  };
   return (
     <button
       type="button"
       className="nes-btn directional-button"
-      dangerouslySetInnerHTML={setDirection()}
       onClick={clickHandler}
-    />
+    >
+      {props.direction}
+    </button>
   );
 }
 

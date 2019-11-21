2e1986e5069f8231981d37bde978385a622eae06
Courtney Buratto <courtneyburatto@Courtneys-MacBook-Pro.local>
2019-11-20 22:37:19 -0700

    update console logs to js format

--- src/functions/miner.js
+++ src/functions/miner.js
@@ -3,12 +3,12 @@ var sha256 = require('js-sha256').sha256;
 
 
 export const proof_of_work = (last_proof, difficulty) => {
-  // console.log(“Searching for next proof”)
+  console.log('Searching for next proof')
   let proof = 0
   while (valid_proof(last_proof, proof, difficulty) === false) {
       proof += 1
   }
-  // console.log(“Proof found: ” + proof)
+  console.log(`Proof found: ${proof}`)
   return proof
 }
   
